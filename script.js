1d4e282f9a232ebbcd705ce131e8c13c1709870f
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-23 19:21:29 +0300

    дз16

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ class First{
 }
 
 class Second extends First{
-    helloReport(){
-        console.log('А я наследуемый метод!');
+    hello(){
+        super.hello()
+        console.log('А я наследуемый!');
     }
 }
 
-const first1 = new First();
-first1.hello();
 const second2 = new Second();
 second2.hello();
-second2.helloReport();
