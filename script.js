d2f009e80259e14bd95cf323e6b5abe692530955
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-26 16:49:59 +0300

    дз18 часть 2

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 'use strict';
 
-class First{
-    hello(){
-        console.log('Привет я метод родителя!');
-    }
-}
+let date = new Date();
+let date1 = new Date(2021, 0, 1)
+console.log(date.getHours());
 
-class Second extends First{
-    hello(){
-        super.hello()
-        console.log('А я наследуемый!');
-    }
-}
+function hi(date){
+(date.getHours()) > 0 && (date.getHours()) <= 6 ? console.log('Доброй ночи!') : 
+(date.getHours()) > 6 && (date.getHours()) <= 10 ? console.log('Доброe утро!') :
+(date.getHours()) > 10 && (date.getHours()) <= 16 ? console.log('Добрый день!') :
+(date.getHours()) > 16 && (date.getHours()) < 24 ? console.log('Добрый вечер!'):
+console.log();
+};
 
-const second2 = new Second();
-second2.hello();
+hi(date);
+
+function getWeekDay(date){
+  let day = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+  return day[date.getDay()];
+};
+console.log('Сегодня: ', getWeekDay(date));
+console.log('Текущее время: ' + date.toLocaleTimeString('en'));
+
+// function getNewYearDay (date){
+//   let newYearDay = new Date(2021, 0, 1) - ;
+// };
+let newYearDay = new Date(2021, 0, 1);
+console.log('Новый год через: ' + Math.floor(((date1.getTime() - Date.now())) /(1000 * 3600 * 24)) + ' дней');
